b6c04e1a288819a8e43b56d83dfe2c228d78b016
TruongChiLinh <linhtruong5427@gmail.com>
2024-07-23 19:18:07 +0700

    first commit

---
+++ src/main/java/com/example/demo_devops_be/DemoDevopsBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_devops_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDevopsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDevopsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_devops_be/DemoDevopsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_devops_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDevopsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
